0a76f81cd6baca18304ed29974f391a68de2e771
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-04-23 19:28:27 +0530

    update3

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
             <article className="about__card">
               <FiUsers className="about__icon" />
               <h5>Company</h5>
-              <small>LetsGrowMore</small>
+              <small>LetsGrowMore</small><br/>
               <small>Developer Abode</small>
             </article>
             <article className="about__card">
